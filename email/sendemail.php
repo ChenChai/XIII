bacf46dacbd4e9f4fe6f53d8b4bc0697aaaa9fda
ChenChai <chen.chai@outlook.com>
2019-03-10 13:58:01 -0400

    Added POST interface for Edwin to call to write and read blocks from database

--- email/sendemail.php
+++ email/sendemail.php
@@ -1,7 +1,12 @@
+
 <?php
+    // This script takes and id and block from POST and adds it to the table of blocks in the database.
     include "../database/connectdb.php";
-    $id = 1;
-    $block = "hi";
+    
+    // given from python script
+    $id = $_POST["id"];
+    $block = $_POST["block"];
+
     if (true === $connection->query("INSERT INTO blocks (id, block) VALUES($id, '$block')")) {
         echo "Block created!";
     } else {
