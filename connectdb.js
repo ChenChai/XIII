057250652b8dc334d9ff89e13e235b74ab886cbd
Chen Chai <chen.chai@outlook.com>
2019-03-10 16:52:24 +0000

    added connectdb.js

---
+++ connectdb.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection( {
+	host: "localhost",
+	user: "guest",
+	password: "password"
+
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log("Connection success!");
+});
