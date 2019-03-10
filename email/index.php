b73335e3dc777b4a590b8f03582688e0f873b29f
ChenChai <chen.chai@outlook.com>
2019-03-10 18:03:46 -0400

    Message displays in inbox

--- email/index.php
+++ email/index.php
@@ -34,7 +34,8 @@
                 <?php
                     foreach($emails["msg"] as $email) {
                         echo '<div class="message">';
-                        echo '<p class="subject">'.$email["sub"]."</p>";
+                        echo '<h3 class="subject">'.$email["sub"]."</p>";
+                        echo '<p class="subject">'.$email["msg"]."</p>";
                         echo '</div>';
                     }
                 ?>
