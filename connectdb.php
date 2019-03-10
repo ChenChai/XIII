ba28bcb941990f394c2df2ba85bbec108c7f121c
Chen Chai <chen.chai@outlook.com>
2019-03-10 16:06:32 +0000

    added SQL connection

--- connectdb.php
+++ connectdb.php
@@ -1,11 +1,20 @@
+
+<html>
+<head></head>
+ping
 <?php
+
 
 $server = 'localhost';
 $user = 'root';
 $password = 'Terrible.hack';
-$db = 'users'
+$db = 'users';
 
-$connection = mysql_connect($server, $user, $password);
+$connection = new mysqli($server, $user, $password, $db);
+
+//$connection = mysql_connect($server, $user, $password);
+/*
+
 or die ("Failed to connect to server! \n") . mysql_error());
 mysql_select_db($db);
 or die ("Failed to connect to server! \n") . mysql_error());
@@ -15,6 +24,7 @@ $result = mysql_query("SELECT * FROM test");
 foreach (mysql_fetch_array($result) as $row) {
     echo $row['name'] . '<br/>';
 }
-
-
+ */
 ?>
+hi
+</html>
