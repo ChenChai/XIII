2b3f9b57f8d195cb23dd16e37b8aa783a78ffb66
ChenChai <chen.chai@outlook.com>
2019-03-10 15:40:57 -0400

    Implemented login properly

--- login/index.php
+++ login/index.php
@@ -1,38 +1,40 @@
 <?php
     include '../database/connectdb.php';
-    $username = "test"; // $_POST["userName"];
-    $password = "testPass"; //  $_POST["password"];
+    $username = $_POST["userName"];
+    $password = $_POST["password"];
 
-    $hashed = $password; //password_hash($password, PASSWORD_DEFAULT, array());
-    $result = $connection->query("SELECT * FROM users"); // get username
+    $hashed = password_hash($password, PASSWORD_DEFAULT, array());
+    $result = $connection->query("SELECT * FROM users WHERE username='$username'"); // get username
 
     if ($result === false) {
 	echo "Database Query Error: " . $mysqli->error;
     }
 
-    $row = reset($result->fetch_assoc()); // returns first value in associative array, or false if empty
-
-    if ($row === false) { // user not found in database.
-	// attempt to create new account
+    if ($result->num_rows === 0) {
+        // user not found in database.
+        // attempt to create new account
         if (true === $connection->query("INSERT INTO users (username, password) VALUES('$username', '$hashed')")) {
             echo "New Account Created. Welcome, ". $username;
         } else {
             echo "Account creation failed!";
         }
-
-    } else { // check if password good
-        if ($row["password"] === $hashed) {
-            // user's password correct!
-            echo "Welcomed, " . $username;
-        } else {
-            echo "Password incorrect!";
-        }
     }
 
+    while ($row = $result->fetch_assoc()) { // returns first value in associative array, or false if empty
+
+        if ($row === false) { 
+
+        } else { // check if password good
+            if (password_verify($password, $row["password"])) {
+                // user's password correct!
+                echo "Your password is correct! Welcome, " . $username;
+            } else {
+                echo "Password incorrect! Please go back and try again.";
+            }
+        }
+        break;
+    }
 ?>
 <html>
-<br/><br/><br/>
-<?php echo $_POST["userName"]; ?>
-<br/> Your password was : <?php echo $_POST["password"]; ?>
 
 </html>
