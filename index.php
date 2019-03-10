a9021168cec78f5fffe9074c774a1e4a4acbf120
jshanneson <45860628+jshanneson@users.noreply.github.com>
2019-03-10 15:04:11 -0400

    formatting login

--- index.php
+++ index.php
@@ -2,26 +2,26 @@
 <html>
 
 <head>
-    <title>DATING LOGIN</title>
+    <title>LOGIN</title>
     <script src="script.js"></script>
 	<link rel="stylesheet" type="text/css" href="styles.css"/>
 </head>
 
-<body>
-    <div class="footer" style="margin-top: auto;">
-        Created by Edwin, Joshua and Chen.
+    <body>
+        <div class="container">
+            <p id="welcome"></p>
 
-        <p id="welcome"></p>
+            <h1>Login</h1>
 
-        <h1>Login</h1>
-        
-        
-        <form id="login" action="./login/index.php" method="post">
-            UserName: <br/><input type="text" name="userName" id="userName"/><br/><br/>
-            Password: <br/><input type="text" name="password" id="password"/><br/><br/>
-            <input type="submit" id="enter" value="Submit" onclick="createVariables();"/>
 
-        </form>
-    </div>
-</body>
+            <form id="login" action="./login/index.php" method="post">
+                UserName: <br /><input type="text" name="userName" id="userName" /><br /><br />
+                Password: <br /><input type="text" name="password" id="password" /><br /><br />
+                <input type="submit" id="enter" value="Submit" />
+
+            </form>
+
+            <div class="footer"><br />Created by Edwin, Joshua and Chen.</div>
+        </div>
+    </body>
 </html>
